f431c2f96089b472f48d32e0d4a4321c337d29bd
Nisan <nisani1239@gmail.com>
2022-07-19 21:36:52 +0300

    ad

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,24 @@
 </template>
 
 <script>
-  import { store } from './store/store';
-  import appHeader from './cmps/app-header.vue';
-  // import userMsg from './cmps/user-msg.vue';
-  import appFooter from './cmps/app-footer.vue';
-  // import { userService } from './services/user.service'
+import { store } from './store/store';
+import appHeader from './cmps/app-header.vue';
+// import userMsg from './cmps/user-msg.vue';
+import appFooter from './cmps/app-footer.vue';
+// import { userService } from './services/user.service'
 
-  export default {
-    created() {
-      console.log('Vue App created');
-      // const user = userService.getLoggedinUser()
-      // if (user) store.commit({ type: 'setLoggedinUser', user })
-    },
-    components: {
-      appHeader,
-      appFooter,
-      store,
-      // userMsg,
-    },
-  };
+export default {
+  created() {
+    this.$store.dispatch({ type: "loadStays" })
+    console.log('Vue App created');
+    // const user = userService.getLoggedinUser()
+    // if (user) store.commit({ type: 'setLoggedinUser', user })
+  },
+  components: {
+    appHeader,
+    appFooter,
+    store,
+    // userMsg,
+  },
+};
 </script>
