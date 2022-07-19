8446f0df5c55d6642ae5df6b94df406912b4b1e1
tztomer <mytztomer@gmail.com>
2022-07-19 20:51:01 +0300

    remove sockets

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,23 @@
 </template>
 
 <script>
+  import { store } from './store/store';
+  import appHeader from './cmps/app-header.vue';
+  // import userMsg from './cmps/user-msg.vue';
+  import appFooter from './cmps/app-footer.vue';
+  // import { userService } from './services/user.service'
 
-
-import { store } from './store/store'
-import appHeader from './cmps/app-header.vue'
-import userMsg from './cmps/user-msg.vue'
-import appFooter from './cmps/app-footer.vue'
-import { userService } from './services/user.service'
-import AppFooter from './cmps/app-footer.vue'
-
-
-export default {
-
-  created() {
-    console.log('Vue App created')
-    const user = userService.getLoggedinUser()
-    if (user) store.commit({ type: 'setLoggedinUser', user })
-  },
-  components: {
-    appHeader,
-    appFooter,
-    userMsg,
-    AppFooter
-  },
-}
+  export default {
+    created() {
+      console.log('Vue App created');
+      // const user = userService.getLoggedinUser()
+      // if (user) store.commit({ type: 'setLoggedinUser', user })
+    },
+    components: {
+      appHeader,
+      appFooter,
+      store,
+      // userMsg,
+    },
+  };
 </script>
