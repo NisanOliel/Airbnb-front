d20686973418e29c05a72e540b68bc581b592dc3
Nisan <nisani1239@gmail.com>
2022-07-19 23:00:53 +0300

    .

--- src/cmps/preview-stay.cmp.vue
+++ src/cmps/preview-stay.cmp.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="preview">
-    <!-- <pre>
+    <pre>
   {{ stay }}
-  </pre
-    > -->
+  </pre>
+    <!-- <img v-for="image in stay.imgUrls" src="../assets/Images/" {{stay.imgUrls}}> -->
     <div>{{ stay.propertyType }} in {{ stay.address.city }}, {{ stay.address.country }}</div>
+    <div>{{ stay.name }} </div>
+    <div>{{ stay.beds }} beds </div>
+    <div>{{ stay.price }}$ / night </div>||
     <!-- <div>{{ stay.price }}</div> -->
 
     <!-- <router-link :to="'/toy/details/' + toy._id">Details</router-link>
@@ -13,10 +16,10 @@
 </template>
 
 <script>
-  export default {
-    name: 'stay-preview',
-    props: {
-      stay: Object,
-    },
-  };
+export default {
+  name: 'stay-preview',
+  props: {
+    stay: Object,
+  },
+};
 </script>
