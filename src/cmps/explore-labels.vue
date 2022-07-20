6c2cc3b2b82d0c31dca191096cccf04ab4ff911e
Asaf Miller <asafm94@gmail.com>
2022-07-20 18:04:53 +0300

    label filter

--- src/cmps/explore-labels.vue
+++ src/cmps/explore-labels.vue
@@ -15,9 +15,8 @@ export default {
         `src/assets/app-filter-img/33848f9e-8dd6-4777-b905-ed38342bacb9.jpg`,
         `src/assets/app-filter-img/33848f9e-8dd6-4777-b905-ed38342bacb9.jpg`,
         `src/assets/app-filter-img/33848f9e-8dd6-4777-b905-ed38342bacb9.jpg`,
-      ],
-    };
-
+      ]
+    }
   }
 }
 </script>
