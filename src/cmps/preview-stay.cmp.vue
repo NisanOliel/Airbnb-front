ccbe8e917802af9c253c458c2e6ae0ecf642221e
tztomer <mytztomer@gmail.com>
2022-07-20 11:38:40 +0300

    remove pre

--- src/cmps/preview-stay.cmp.vue
+++ src/cmps/preview-stay.cmp.vue
@@ -1,20 +1,17 @@
-
 <template>
   <div class="preview">
     <!-- <pre>
   {{ stay }}
   </pre> -->
 
-    <pre>{{ images }}</pre>
-
+    <!-- <pre>{{ images }}</pre> -->
 
     <div>
       <el-carousel trigger="click" height="150px">
-        <el-carousel-item v-for="image in  images " :key="image">
+        <el-carousel-item v-for="image in images" :key="image">
           <h3 class="small justify-center" text="2xl">{{ image }}</h3>
         </el-carousel-item>
       </el-carousel>
-
     </div>
 
     <!-- <img v-for="image in stay.imgUrls" :src="`src/assets/Images/${image}`"> -->
@@ -30,22 +27,20 @@
 </template>
 
 <script>
-export default {
-  name: 'stay-preview',
-  props: {
-    stay: Object,
-  },
-  data() {
-    return {
-      images: [
-        `src/assets/Images/${this.stay.imgUrls[0]}`,
-        `src/assets/Images/${this.stay.imgUrls[1]}`,
-        `src/assets/Images/${this.stay.imgUrls[2]}`,
-        `src/assets/Images/${this.stay.imgUrls[3]}`
-      ]
-
-    }
-  },
-};
+  export default {
+    name: 'stay-preview',
+    props: {
+      stay: Object,
+    },
+    data() {
+      return {
+        images: [
+          `src/assets/Images/${this.stay.imgUrls[0]}`,
+          `src/assets/Images/${this.stay.imgUrls[1]}`,
+          `src/assets/Images/${this.stay.imgUrls[2]}`,
+          `src/assets/Images/${this.stay.imgUrls[3]}`,
+        ],
+      };
+    },
+  };
 </script>
-
