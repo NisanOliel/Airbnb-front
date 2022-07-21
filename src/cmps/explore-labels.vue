661fe8fd77bb22957d78e452585f606ba0a6684f
tztomer <mytztomer@gmail.com>
2022-07-21 20:57:18 +0300

    replace h2 with span

--- src/cmps/explore-labels.vue
+++ src/cmps/explore-labels.vue
@@ -1,38 +1,37 @@
 <template>
   <ul class="flex text-center">
-    <li v-for=" label in labels" :key="label.src" @click="filter(label.propertyType)" class="labels">
+    <li v-for="label in labels" :key="label.src" @click="filter(label.propertyType)" class="labels">
       <img :src="label.src" />
-      <h2>{{ label.propertyType }}</h2>
+      <span>{{ label.propertyType }}</span>
     </li>
   </ul>
 </template>
 <script>
-export default {
-  name: 'explore-labels',
-  data() {
-    return {
-      labels: '',
-      filterBy: {
-        label: null
+  export default {
+    name: 'explore-labels',
+    data() {
+      return {
+        labels: '',
+        filterBy: {
+          label: null,
+        },
+      };
+    },
+    methods: {
+      filter(value) {
+        this.filterBy.label = value;
+        this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy });
+        console.log(this.filterBy.label);
       },
-    }
-  },
-  methods: {
-    filter(value) {
-      this.filterBy.label = value
-      this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy })
-      console.log(this.filterBy.label);
-    }
-  },
+    },
 
-  created() {
-    this.labels = this.$store.getters.getLabels
-
-  }, computed: {
-    getLabels() {
-      return this.labels;
-    }
-  }
-
-}
+    created() {
+      this.labels = this.$store.getters.getLabels;
+    },
+    computed: {
+      getLabels() {
+        return this.labels;
+      },
+    },
+  };
 </script>
