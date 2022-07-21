1f7f39a6085a28baa7d80e11ad638db0eec77549
tztomer <mytztomer@gmail.com>
2022-07-21 23:40:31 +0300

    font

--- src/cmps/standAlone-filter.vue
+++ src/cmps/standAlone-filter.vue
@@ -1,35 +1,33 @@
 <template>
-    <form>
-        <button>filter</button>
-    </form>
+  <form>
+    <button>filter</button>
+  </form>
 </template>
 <script>
-export default {
-
+  export default {
     data() {
-        return {
-            // labels: '',
-            // filterBy: {
-            //     label: null
-            // },
-        }
+      return {
+        // labels: '',
+        // filterBy: {
+        //     label: null
+        // },
+      };
     },
     methods: {
-        // filter(value) {
-        //     this.filterBy.label = value
-        //     this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy })
-        //     console.log(this.filterBy.label);
-        // }
+      // filter(value) {
+      //     this.filterBy.label = value
+      //     this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy })
+      //     console.log(this.filterBy.label);
+      // }
     },
 
     created() {
-        // this.labels = this.$store.getters.getLabels
-
-    }, computed: {
-        // getLabels() {
-        //     return this.labels;
-        // }
-    }
-
-}
+      // this.labels = this.$store.getters.getLabels
+    },
+    computed: {
+      // getLabels() {
+      //     return this.labels;
+      // }
+    },
+  };
 </script>
