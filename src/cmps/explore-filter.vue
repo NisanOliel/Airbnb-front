6a9a29a6e9d007319a3a3e28bae4c528380ddda6
tztomer <mytztomer@gmail.com>
2022-07-22 21:20:32 +0300

    main filter header

--- src/cmps/explore-filter.vue
+++ src/cmps/explore-filter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="search-wrapper flex">
+  <div class="search-wrapper flex" :class="{ expend: !filterPreview }">
     <div v-show="filterPreview" @click="expendForm($event)" class="filter-preview flex align-center">
       <div class="filter btn header-location">Anywhere</div>
       <div class="filter btn header-time">Any week</div>
@@ -11,32 +11,155 @@
     <div v-show="!filterPreview" class="filter-expend flex">
       <el-form :model="form" @submit.prevent="formSubmit">
         <div class="filter-option">
-          <el-form-item label="Where">
-            <el-input @focus="showInitModal($event)" v-model="form.where" placeholder="Search destination" />
-          </el-form-item>
+          <label for="where">Where</label>
+          <input name="where" @focus="showInitModal($event)" v-model="form.where" placeholder="Search destination" />
+
           <div v-if="showModal">dflkgjldfkjgdlsfgjdfslkgjdfsljl</div>
         </div>
         <div @click="activeDateModal" class="filter-option check">
           <div class="labels-wrap">
-            <span>Check in</span>
-            <span>Check out</span>
+            <v-date-picker :columns="2" v-model="form.date" is-range>
+              <template v-slot="{ inputValue, inputEvents }">
+                <div class="flex justify-center items-center">
+                  <div class="checkin">
+                    <label for="checkin">Check in</label>
+                    <input
+                      name="checkin"
+                      :value="inputValue.start"
+                      v-on="inputEvents.start"
+                      placeholder="Add dates"
+                      class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                    />
+                  </div>
+                  <div class="checkout">
+                    <label for="checkout">Check out</label>
+                    <input
+                      name="checkout"
+                      :value="inputValue.end"
+                      v-on="inputEvents.end"
+                      placeholder="Add dates"
+                      class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                    />
+                  </div>
+                </div>
+              </template>
+            </v-date-picker>
+          </div>
+        </div>
+        <div class="filter-option guest-dropdown">
+          <div class="add-guest-wrapper">
+            <label for="add-guest">Who</label>
+            <input disabled type="text" placeholder="Add guests" />
           </div>
 
-          <el-date-picker
-            ref="datePicker"
-            v-model="form.date"
-            type="daterange"
-            :default-time="defaultTime"
-            start-placeholder="Add dates"
-            end-placeholder="Add dates"
-            format="MMM D"
-            range-separator=""
-          />
-
-          <!-- {{ form.date }} -->
-        </div>
-        <div class="filter-option">
-          <button><img class="search" src="src/assets/search-icon.svg" /></button>
+          <div class="order-container header-filter-search">
+            <div class="btn-container on-filter">
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="content filter-content">
+                <button class="action-btn">
+                  <div class="material-icons search"> search </div>
+                  <span>Search</span>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </el-form>
     </div>
