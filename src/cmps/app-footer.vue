5049a2764c9115c41db2526bcb0a2d1a9fcd1950
Nisan <nisani1239@gmail.com>
2022-07-22 14:34:41 +0300

    footer

--- src/cmps/app-footer.vue
+++ src/cmps/app-footer.vue
@@ -1,17 +1,26 @@
 <template>
-  <section class="app-footer">
-    <div class="left-side">
-      <span>© 2022 Air2B,inc</span>
-      <span>·</span>
-      <span>Privacy</span>
-      <span>·</span>
-      <span>Terms</span>
-      <span>·</span>
-      <span>Sitemap</span>
-    </div>
+  <section class="app-footer ">
+    <div class="container flex justify-space-between">
 
-    <div class="right-side">
-      <img src="../assets/icons/en.svg" style="height: 15px;">
+      <div class="left-side">
+        <span>© 2022 Air2B,inc</span>
+        <span>·</span>
+        <span>Privacy</span>
+        <span>·</span>
+        <span>Terms</span>
+        <span>·</span>
+        <span>Sitemap</span>
+      </div>
+
+      <div class="right-side">
+        <img src="../assets/icons/en.svg" style="height: 15px;">
+        <span>English (US)</span>
+        <span>$ USD</span>
+        <span>Support & resources</span>
+        <span class="material-icons-two-tone">expand_less</span>
+
+      </div>
+
     </div>
   </section>
 </template>
