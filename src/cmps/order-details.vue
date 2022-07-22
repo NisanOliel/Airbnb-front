8290719e13e019e6ab381eda9ff785990d3647d5
Nisan <nisani1239@gmail.com>
2022-07-22 18:36:02 +0300

    order css

--- src/cmps/order-details.vue
+++ src/cmps/order-details.vue
@@ -8,18 +8,32 @@
 
 
 
-
+    <!-- Date !! -->
     <div class="order-data">
-      <div class="date-picker">
-        <div class="date-input">
-          <label>check in</label>
-          <input :placeholder="checkIn" />
-        </div>
-        <div class="date-input">
-          <label>check out</label>
-          <input :placeholder="checkOut" />
-        </div>
-      </div>
+      <v-date-picker v-model="trip.dates" is-range :columns="2">
+        <template v-slot="{ inputValue, inputEvents }">
+          <div class="flex justify-center items-center">
+
+            <div class="date-picker">
+              <div class="date-input">
+                <label>check in</label>
+                <input :placeholder="checkIn" :value="inputValue.start" v-on="inputEvents.start"
+                  class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300" />
+              </div>
+
+
+              <div class="date-input">
+                <label>check out</label>
+                <input :placeholder="checkOut" :value="inputValue.end" v-on="inputEvents.end"
+                  class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300" />
+              </div>
+            </div>
+
+          </div>
+        </template>
+      </v-date-picker>
+      <!--  -->
+
 
       <div @click="isShow = !isShow" class="guest-input">
         <label>guests
@@ -32,6 +46,37 @@
         <input disabled :placeholder="guestsCount" />
       </div>
     </div>
+
+
+
+
+    <!-- <div class="order-data">
+      <div class="date-picker">
+        <div class="date-input">
+          <label>check in</label>
+          <input :placeholder="checkIn" />
+        </div>
+        <div class="date-input">
+          <label>check out</label>
+          <input :placeholder="checkOut" />
+        </div>
+      </div>
+      <div @click="isShow = !isShow" class="guest-input">
+        <label>guests
+          <div class="expand-order">
+            <span class="material-icons-outlined" :class="{ flip: !isShow }">
+              expand_less
+            </span>
+          </div>
+        </label>
+        <input disabled :placeholder="guestsCount" />
+      </div>
+    </div> -->
+
+
+
+
+
 
     <div class="cell"></div>
     <div class="cell"></div>
@@ -174,20 +219,24 @@
         </div>
       </div>
 
-      <div class="pricing" if="">
-        <h4>You won't be charged yet</h4>
-        <p>
-          <span>Total</span><span> ${{ totalPrice }}</span>
-        </p>
-      </div>
     </div>
+    <pre>{{ trip.dates }}</pre>
+
+    <div class="pricing" v-if="trip.dates">
+      <h4>You won't be charged yet</h4>
+      <p>
+        <span>Total</span><span> ${{ totalPrice }}</span>
+      </p>
+    </div>
+
   </section>
 </template>
 
 <script>
-// import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
 
 export default {
+
   name: ' order-details',
   props: { stay: { type: Object } },
   data() {
@@ -206,6 +255,7 @@ export default {
     reviewsCount() {
       return this.stay.reviews.length;
     },
+
     guestsCount() {
       const guestsCount = this.trip.guests.children + this.trip.guests.adults;
       if (guestsCount >= 1) return guestsCount + ' guests';
@@ -218,9 +268,12 @@ export default {
       return this.trip.dates[1];
     },
     totalPrice() {
-      var size = Object.keys(this.trip.dates).length;
+      let size = Object.keys(this.trip.dates).length;
       if (size > 1) {
-        const timeDiff = (this.trip.dates[1].getTime() - this.trip.dates[0].getTime()) / (1000 * 3600 * 24);
+        const time = JSON.parse(JSON.stringify(this.trip.dates))
+        const { start, end } = time
+
+        const timeDiff = (new Date(end).getTime() - new Date(start).getTime()) / (1000 * 3600 * 24);
         return Number(parseInt(this.stay.price * timeDiff)).toLocaleString();
       }
     },
@@ -228,15 +281,13 @@ export default {
   },
   methods: {
     updateGuests(type, number) {
-      console.log('number:', number);
-      console.log('type:', type);
+      const guestsCount = this.trip.guests.children + this.trip.guests.adults;
       if (this.trip.guests[type] === 0 && number === -1) return;
+      if (this.stay.capacity === guestsCount && number == 1) return ElMessage.error('You over the guests capacity')
+
       this.trip.guests[type] += number;
     },
-    updateChildren(number) {
-      if (this.trip.guests.children === 0 && number === -1) return;
-      this.trip.guests.children += number;
-    },
+
   },
 };
 </script>
