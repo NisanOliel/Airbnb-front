32a0fd8bf285a3d8bdb99af20b484d2a276ada17
Nisan <nisani1239@gmail.com>
2022-07-23 20:03:51 +0300

    add bold

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -16,36 +16,37 @@
       </div>
       <div>{{ stay.name }} </div>
       <div>{{ stay.beds }} beds </div>
-      <div>£{{ stay.price }} night </div>
+      <div><span class="bold">£{{ stay.price }}</span> night </div>
+
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'stay-preview',
-    props: {
-      stay: Object,
+export default {
+  name: 'stay-preview',
+  props: {
+    stay: Object,
+  },
+  data() {
+    return {
+      images: [
+        `src/assets/Images/${this.stay.imgUrls[0]}`,
+        `src/assets/Images/${this.stay.imgUrls[1]}`,
+        `src/assets/Images/${this.stay.imgUrls[2]}`,
+        `src/assets/Images/${this.stay.imgUrls[3]}`,
+      ],
+      isLiked: false,
+    };
+  },
+  methods: {
+    goToDetails() {
+      this.$router.push('/stay/' + this.stay._id);
     },
-    data() {
-      return {
-        images: [
-          `src/assets/Images/${this.stay.imgUrls[0]}`,
-          `src/assets/Images/${this.stay.imgUrls[1]}`,
-          `src/assets/Images/${this.stay.imgUrls[2]}`,
-          `src/assets/Images/${this.stay.imgUrls[3]}`,
-        ],
-        isLiked: false,
-      };
+    toggleWishList() {
+      this.isLiked = !this.isLiked;
+      console.log('heart is active', this.isLiked);
     },
-    methods: {
-      goToDetails() {
-        this.$router.push('/stay/' + this.stay._id);
-      },
-      toggleWishList() {
-        this.isLiked = !this.isLiked;
-        console.log('heart is active', this.isLiked);
-      },
-    },
-  };
+  },
+};
 </script>
