917b6b7d1bd07b9e96444ba484b776b73e2006b4
Nisan <nisani1239@gmail.com>
2022-07-23 17:28:14 +0300

    explore

---
+++ src/views/stay-explore.vue
@@ -0,0 +1,40 @@
+<template>
+  <h3>exploreee page</h3>
+</template>
+
+<script>
+
+export default {
+  name: 'explore-page',
+  data() {
+    return {
+      filterBy: null
+    };
+  },
+  computed: {
+    stays() {
+      // return this.$store.getters.getFilteredStays;
+    },
+  },
+  created() {
+    // this.$store.dispatch({ type: 'loadStays' });
+  },
+  watch: {
+    $route: {
+      handler() {
+        const { where, text } = this.$route.query;
+        console.log(' this.$route.query:', this.$route.query)
+        console.log('where:', where)
+        console.log('text:', text)
+        // this.filterBy.destination = where;
+        // this.filterBy.name = text;
+        // console.log('this.filterBy:', this.filterBy)
+      },
+      immediate: true,
+    },
+  },
+  methods: {},
+  components: {
+  },
+};
+</script>
