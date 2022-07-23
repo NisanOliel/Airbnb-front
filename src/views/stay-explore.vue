98aae8290f41fa0fa66eae54eab7038a4e004f0a
Nisan <nisani1239@gmail.com>
2022-07-23 19:27:22 +0300

    explore

--- src/views/stay-explore.vue
+++ src/views/stay-explore.vue
@@ -1,40 +1,44 @@
 <template>
-  <h3>exploreee page</h3>
-
-  <pre>{{ stays[0] }}</pre>
-  <pre>THIS NOT EXIST {{ stays[1] }}</pre>
+  <section class="container">
+    <stay-list :stays="stays"></stay-list>
+  </section>
+  <!-- <pre>{{ stays }}</pre> -->
 </template>
 
 <script>
-  export default {
-    name: 'explore-page',
-    data() {
-      return {
-        filterBy: {
-          // location: null,
-        },
-      };
-    },
-    computed: {
-      stays() {
-        // return this.$store.getters.getFilteredStays;
-        return this.$store.getters.getFilteredStays;
+import stayList from '../cmps/stay-list.cmp.vue';
+
+export default {
+  name: 'explore-page',
+  data() {
+    return {
+      stays: null,
+      filterBy: {
       },
-    },
-    created() {
-      this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy });
-    },
-    watch: {
-      $route: {
-        handler() {
-          const { location } = this.$route.query;
-          this.filterBy.location = location;
-          console.log(' this.$route.query:', this.$route.query);
-        },
-        immediate: true,
+    };
+  },
+  computed: {
+    // getStays() {
+    //   this.stays = this.$store.getters.getFilteredStays;
+    // },
+  },
+  created() {
+    this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy });
+    this.stays = this.$store.getters.getFilteredStays;
+  },
+  watch: {
+    $route: {
+      handler() {
+        const { location } = this.$route.query;
+        this.filterBy.location = location;
+        console.log(' this.$route.query:', this.$route.query);
       },
+      immediate: true,
     },
-    methods: {},
-    components: {},
-  };
+  },
+  methods: {},
+  components: {
+    stayList
+  },
+};
 </script>
