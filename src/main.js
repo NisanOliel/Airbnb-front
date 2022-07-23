906be9b2a08f759c27dcfa5ad6e7965a60118c9d
Nisan <nisani1239@gmail.com>
2022-07-23 23:56:40 +0300

    a

--- src/main.js
+++ src/main.js
@@ -8,12 +8,6 @@ import 'element-plus/dist/index.css';
 
 import VCalendar from 'v-calendar';
 
-<<<<<<< HEAD
-import VueClickAway from 'vue3-click-away';
-
-import './assets/styles/main.scss';
-import App from './App.vue';
-=======
 import VueClickAway from "vue3-click-away";
 
 import './assets/styles/main.scss';
@@ -23,7 +17,6 @@ import App from './App.vue';
 import HistogramSlider from "vue3-histogram-slider";
 import "vue3-histogram-slider/dist/histogram-slider.css";
 
->>>>>>> e2e6072875b2cce4087f87013f22a5c0bb03d502
 const app = createApp(App);
 
 app.config.globalProperties.$filters = {
@@ -39,15 +32,9 @@ app.config.globalProperties.$filters = {
 app.use(router);
 app.use(store);
 app.use(ElementPlus);
-<<<<<<< HEAD
-
-app.use(VCalendar, {});
-app.use(VueClickAway);
-=======
 app.use(VCalendar, {})
 app.use(VueClickAway)
 // app.use(materialIcons);
 app.component(HistogramSlider.name, HistogramSlider);
->>>>>>> e2e6072875b2cce4087f87013f22a5c0bb03d502
 
 app.mount('#app');
