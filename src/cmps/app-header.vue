88767ecf2e172a1a250d95a53d5f46602f0ea521
Nisan <nisani1239@gmail.com>
2022-07-24 02:09:11 +0300

    a

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -13,11 +13,6 @@
           <explore-filter />
         </div>
 
-<<<<<<< HEAD
-=======
-        <a class="right-header" href="/#/login"><img class="explore-btn" src="../assets/icons/en.svg" /></a>
->>>>>>> 7993479f092cf74129d32dc1103fca39c6141fa1
-
         <div class="flex">
           <a class="right-header" href="/explore">Explore</a>
           <a class="right-header" href="/#/login"><img class="explore-btn" src="../assets/icons/en.svg" /></a>
@@ -32,22 +27,14 @@
       <div v-if="headerLocation" class="header-labels flex justify-space-between align-center">
         <explore-labels />
         <div class="stand-alone-filter">
-          <button @click="isShow = !isShow" style="--filter-button_border: 1px solid var(--j-qkgmf)" type="button" class="v1tureqs dir dir-ltr"
-            ><span class="i3c9txn dir dir-ltr"
-              ><svg
-                viewBox="0 0 16 16"
-                xmlns="http://www.w3.org/2000/svg"
-                style="display: block; height: 14px; width: 14px; fill: currentColor"
-                aria-hidden="true"
-                role="presentation"
-                focusable="false"
-              >
+          <button @click="isShow = !isShow" style="--filter-button_border: 1px solid var(--j-qkgmf)" type="button"
+            class="v1tureqs dir dir-ltr"><span class="i3c9txn dir dir-ltr"><svg viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg" style="display: block; height: 14px; width: 14px; fill: currentColor"
+                aria-hidden="true" role="presentation" focusable="false">
                 <path
-                  d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
-                ></path></svg
-              ><span class="t1o11edy dir dir-ltr">Filters</span></span
-            ></button
-          >
+                  d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z">
+                </path>
+              </svg><span class="t1o11edy dir dir-ltr">Filters</span></span></button>
         </div>
 
         <standAlone-filter @closeFilersForm="closeModal" v-if="isShow" v-click-away="onClickAway" />
@@ -56,62 +43,59 @@
   </header>
 </template>
 <script>
-  import exploreFilter from './explore-filter.vue';
-  import exploreLabels from './explore-labels.vue';
-  import standAloneFilter from './standAlone-filter.vue';
+import exploreFilter from './explore-filter.vue';
+import exploreLabels from './explore-labels.vue';
+import standAloneFilter from './standAlone-filter.vue';
 
-  export default {
-    data() {
-      return {
-        isShow: false,
-        location: false,
-      };
+export default {
+  data() {
+    return {
+      isShow: false,
+      location: false,
+    };
+  },
+  created() {
+    // const headerLoc = this.$route.path;
+    // const params = this.$route.params;
+    // const isEmpty = Object.keys(params).length === 0;
+    // console.log('heder loc', isEmpty);
+    // this.location = isEmpty;
+  },
+  computed: {
+    // loggedInUser() {
+    //   return this.$store.getters.loggedinUser;
+    // },
+    headerLocation() {
+      let params = this.$route.params;
+      let isEmpty = Object.keys(params).length === 0;
+      return isEmpty;
     },
-    created() {
-      // const headerLoc = this.$route.path;
-      // const params = this.$route.params;
-      // const isEmpty = Object.keys(params).length === 0;
-      // console.log('heder loc', isEmpty);
-      // this.location = isEmpty;
+  },
+  methods: {
+    closeModal() {
+      this.isShow = false;
     },
-    computed: {
-      // loggedInUser() {
-      //   return this.$store.getters.loggedinUser;
-      // },
-      headerLocation() {
-        let params = this.$route.params;
-        let isEmpty = Object.keys(params).length === 0;
-        return isEmpty;
-      },
-    },
-    methods: {
-      closeModal() {
-        this.isShow = false;
-      },
-      onClickAway() {
-        this.isShow = false;
-      },
-
-      closeModal() {
-        this.isShow = false;
-      },
+    onClickAway() {
+      this.isShow = false;
     },
 
-    components: {
-      exploreFilter,
-      exploreLabels,
-      standAloneFilter,
-    },
+  },
 
-    watch: {
-      isShow: function () {
-        if (this.isShow) {
-          document.documentElement.style.overflow = 'hidden';
-          return;
-        }
+  components: {
+    exploreFilter,
+    exploreLabels,
+    standAloneFilter,
+  },
 
-        document.documentElement.style.overflow = 'auto';
-      },
+  watch: {
+    isShow: function () {
+      if (this.isShow) {
+        document.documentElement.style.overflow = 'hidden';
+        return;
+      }
+
+      document.documentElement.style.overflow = 'auto';
     },
-  };
+  },
+};
 </script>
