29549b9036124ecedd769408bf4fc51975300f27
Nisan <nisani1239@gmail.com>
2022-07-24 14:36:54 +0300

    fix css feature

--- src/cmps/features-details.vue
+++ src/cmps/features-details.vue
@@ -1,32 +1,36 @@
 <template>
-  <section class="flex">
-    <div>
-      <img src="../assets/icons/check-in.svg" style="height: 20px;">
-    </div>
-    <div>
-      <h3>Self check-in</h3>
-      <h5>Check yourself in with the lockbox.</h5>
-    </div>
-  </section>
+  <div class="gray-box features-details">
 
-  <section class="flex">
-    <div>
-      <img src="../assets/icons/location.svg" style="height: 20px;">
-    </div>
-    <div>
-      <h3>Great location</h3>
-      <h5>100% of recent guests gave the location a 5-star rating.</h5>
-    </div>
-  </section>
 
-  <section class="flex">
-    <div>
-      <img src="../assets/icons/cancellation.svg" style="height: 20px;">
-    </div>
-    <div>
-      <h3>Free cancellation for 48 hours.</h3>
-    </div>
-  </section>
+    <section class="flex">
+      <div>
+        <img src="../assets/icons/check-in.svg" style="height: 20px;">
+      </div>
+      <div>
+        <h3>Self check-in</h3>
+        <h5>Check yourself in with the lockbox.</h5>
+      </div>
+    </section>
+
+    <section class="flex">
+      <div>
+        <img src="../assets/icons/location.svg" style="height: 20px;">
+      </div>
+      <div>
+        <h3>Great location</h3>
+        <h5>100% of recent guests gave the location a 5-star rating.</h5>
+      </div>
+    </section>
+
+    <section class="flex">
+      <div>
+        <img src="../assets/icons/cancellation.svg" style="height: 20px;">
+      </div>
+      <div>
+        <h3>Free cancellation for 48 hours.</h3>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
