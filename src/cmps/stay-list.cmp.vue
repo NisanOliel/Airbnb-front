7f12559387ae0805d978294e8a568e8440a149ff
Asaf Miller <asafm94@gmail.com>
2022-07-25 12:49:15 +0300

    labels

--- src/cmps/stay-list.cmp.vue
+++ src/cmps/stay-list.cmp.vue
@@ -3,7 +3,6 @@
     <div v-for="stay in stays" class="preview-wrapper">
       <stay-preview :stay="stay" :key="stay._id" />
     </div>
-    <div v-if="stays.length === 0">No Results Had Been Found</div>
   </div>
 </template>
 
