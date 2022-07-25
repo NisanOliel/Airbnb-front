4c5e0c629137d50c8db5b3b35eafcf14a7ae5742
tztomer <mytztomer@gmail.com>
2022-07-25 20:15:35 +0300

    nav logic

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,58 +1,62 @@
 <template>
   <div v-if="isShow" class="overlay"></div>
+
+  <!-- <Transition name="bounce"> </Transition> -->
   <header class="main-header flex column" :class="{ sticky: isSticky }">
-    <div :class="{ container: headerLocation, 'inner-container': !headerLocation }">
-      <nav class="top-nav flex justify-space-between align-center">
-        <router-link to="/">
-          <div class="main-logo flex">
-            <div class=""><img src="src/assets/airbnb-logo.svg" /></div>
-            <h1>air2b</h1>
+    <div class="nav-wrapper" :class="{ expend: isExpend }">
+      <div :class="{ container: headerLocation, 'inner-container': !headerLocation }">
+        <nav class="top-nav flex justify-space-between align-center">
+          <router-link to="/">
+            <div class="main-logo flex">
+              <div class=""><img src="src/assets/airbnb-logo.svg" /></div>
+              <h1>air2b</h1>
+            </div>
+          </router-link>
+          <div class="explore-filter">
+            <explore-filter :isExpend="isExpend" @expend-form="expendForm" />
           </div>
-        </router-link>
-        <div class="explore-filter">
-          <explore-filter />
-        </div>
 
-        <div class="flex align-center">
-          <el-tooltip content="Become a host" placement="bottom" effect="light">
-            <a class="right-header bold become-host" href="/#/login">Become a host</a>
-          </el-tooltip>
+          <div class="flex align-center">
+            <el-tooltip content="Become a host" placement="bottom" effect="light">
+              <a class="right-header bold become-host" href="/#/login">Become a host</a>
+            </el-tooltip>
 
-          <el-tooltip content="Explore" placement="bottom" effect="light">
-            <a class="right-header" href="/#/explore"><img class="explore-btn" src="../assets/icons/en.svg" /></a>
-          </el-tooltip>
+            <el-tooltip content="Explore" placement="bottom" effect="light">
+              <a class="right-header" href="/#/explore"><img class="explore-btn" src="../assets/icons/en.svg" /></a>
+            </el-tooltip>
 
-          <button class="nav-btn expend flex">
-            <img class="menu-btn" src="../assets/icons/menu.svg" />
-            <img class="host-image" src="https://randomuser.me/api/portraits/men/10.jpg" />
-          </button>
-        </div>
-      </nav>
-
-      <div v-if="headerLocation" class="header-labels flex justify-space-between align-center">
-        <explore-labels />
-        <div class="stand-alone-filter">
-          <button @click="isShow = !isShow" style="--filter-button_border: 1px solid var(--j-qkgmf)" type="button" class="v1tureqs dir dir-ltr"
-            ><span class="i3c9txn dir dir-ltr"
-              ><svg
-                viewBox="0 0 16 16"
-                xmlns="http://www.w3.org/2000/svg"
-                style="display: block; height: 14px; width: 14px; fill: currentColor"
-                aria-hidden="true"
-                role="presentation"
-                focusable="false"
-              >
-                <path
-                  d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
-                ></path></svg
-              ><span class="t1o11edy dir dir-ltr">Filters</span></span
-            ></button
-          >
-        </div>
-        <standAlone-filter @closeFilersForm="closeModal" v-if="isShow" v-click-away="onClickAway" />
-        <!-- <Transition duration="550" name="nested">
-        </Transition> -->
+            <button class="nav-btn expend flex">
+              <img class="menu-btn" src="../assets/icons/menu.svg" />
+              <img class="host-image" src="https://randomuser.me/api/portraits/men/10.jpg" />
+            </button>
+          </div>
+        </nav>
       </div>
+    </div>
+
+    <div v-if="headerLocation" class="header-labels flex justify-space-between align-center">
+      <explore-labels />
+      <div class="stand-alone-filter">
+        <button @click="isShow = !isShow" style="--filter-button_border: 1px solid var(--j-qkgmf)" type="button" class="v1tureqs dir dir-ltr"
+          ><span class="i3c9txn dir dir-ltr"
+            ><svg
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+              style="display: block; height: 14px; width: 14px; fill: currentColor"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+            >
+              <path
+                d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+              ></path></svg
+            ><span class="t1o11edy dir dir-ltr">Filters</span></span
+          ></button
+        >
+      </div>
+      <standAlone-filter @closeFilersForm="closeModal" v-if="isShow" v-click-away="onClickAway" />
+      <!-- <Transition duration="550" name="nested">
+        </Transition> -->
     </div>
   </header>
 </template>
@@ -67,12 +71,13 @@
         isShow: false,
         location: false,
         isSticky: false,
+        isExpend: false,
       };
     },
     created() {
       window.addEventListener('scroll', this.handleScroll);
     },
-    destroyed() {
+    beforeUnmount() {
       window.removeEventListener('scroll', this.handleScroll);
     },
     computed: {
@@ -84,6 +89,11 @@
     },
 
     methods: {
+      expendForm() {
+        console.log('expend clickd');
+        // this.filterShow;
+        this.isExpend = !this.isExpend;
+      },
       stickHeader(entries) {
         const [entry] = entries;
         console.log('entry', entry);
@@ -98,8 +108,20 @@
         let pos = window.scrollY;
         if (pos === 0) {
           this.isSticky = false;
-        } else {
+          this.isExpend = false;
+        }
+
+        if (pos > 0) {
           this.isSticky = true;
+        }
+        if (this.isExpend) {
+          let currPos = window.screenY;
+          console.log('currpos', currPos);
+          if (currPos && this.isExpend) console.log('pos', pos);
+          this.isExpend = false;
+          // if (pos > currPos) {
+          //   this.isExpend = false;
+          // }
         }
       },
     },
--- src/cmps/explore-filter.vue
+++ src/cmps/explore-filter.vue
@@ -1,170 +1,174 @@
 <template>
-  <div class="search-wrapper flex" :class="{ expend: !filterPreview }">
-    <div v-show="filterPreview" @click="expendForm($event)" class="filter-preview flex align-center">
+  <div class="search-wrapper flex">
+    <div v-show="!isExpend" @click="expendForm" class="filter-preview flex align-center">
       <div class="filter btn header-location">Anywhere</div>
       <div class="filter btn header-time">Any week</div>
       <div class="filter btn header-guests search" value="hellow"
         >Add guests <span><img src="src/assets/search-icon.svg" /></span>
       </div>
     </div>
-    <Transition name="bounce">
-      <div v-show="!filterPreview" class="filter-expend flex">
-        <el-form :model="filterBy">
-          <div class="filter-option">
-            <label for="where">Where</label>
-            <!-- <input name="where" @focus="showInitModal($event)" v-model="form.where" placeholder="Search destination" /> -->
-            <input name="where" v-model="filterBy.where" placeholder="Search destination" />
 
-            <!-- <div v-if="showModal">dflkgjldfkjgdlsfgjdfslkgjdfsljl</div> -->
-          </div>
-          <div class="filter-option check">
-            <div class="labels-wrap">
-              <v-date-picker :columns="2" v-model="filterBy.date" is-range>
-                <template v-slot="{ inputValue, inputEvents }">
-                  <div class="flex justify-center items-center">
-                    <div class="checkin">
-                      <label for="checkin">Check in</label>
-                      <input
-                        name="checkin"
-                        :value="inputValue.start"
-                        v-on="inputEvents.start"
-                        placeholder="Add dates"
-                        class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
-                      />
-                    </div>
-                    <div class="checkout">
-                      <label for="checkout">Check out</label>
-                      <input
-                        name="checkout"
-                        :value="inputValue.end"
-                        v-on="inputEvents.end"
-                        placeholder="Add dates"
-                        class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
-                      />
-                    </div>
+    <div v-show="isExpend" class="filter-expend flex">
+      <div class="navigation">
+        <a href="http://">Stays</a>
+        <a href="http://">Experiences</a>
+        <a href="http://">Online Experiences</a>
+      </div>
+      <el-form :model="filterBy">
+        <div class="filter-option">
+          <label for="where">Where</label>
+          <!-- <input name="where" @focus="showInitModal($event)" v-model="form.where" placeholder="Search destination" /> -->
+          <input name="where" v-model="filterBy.where" placeholder="Search destination" />
+
+          <!-- <div v-if="showModal">dflkgjldfkjgdlsfgjdfslkgjdfsljl</div> -->
+        </div>
+        <div class="filter-option check">
+          <div class="labels-wrap">
+            <v-date-picker :columns="2" v-model="filterBy.date" is-range>
+              <template v-slot="{ inputValue, inputEvents }">
+                <div class="flex justify-center items-center">
+                  <div class="checkin">
+                    <label for="checkin">Check in</label>
+                    <input
+                      name="checkin"
+                      :value="inputValue.start"
+                      v-on="inputEvents.start"
+                      placeholder="Add dates"
+                      class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                    />
                   </div>
-                </template>
-              </v-date-picker>
-            </div>
-          </div>
-          <div class="filter-option guest-dropdown">
-            <div @click.stop="dropDownMenu($event)" class="add-guest-wrapper">
-              <label for="add-guest">Who</label>
-              <input disabled type="text" placeholder="Add guests" />
-            </div>
-
-            <div @click="formSubmit" class="order-container header-filter-search">
-              <div class="btn-container on-filter">
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="cell"></div>
-                <div class="content filter-content">
-                  <button class="action-btn">
-                    <div class="material-icons search"> search </div>
-                    <span>Search</span>
-                  </button>
+                  <div class="checkout">
+                    <label for="checkout">Check out</label>
+                    <input
+                      name="checkout"
+                      :value="inputValue.end"
+                      v-on="inputEvents.end"
+                      placeholder="Add dates"
+                      class="border px-2 py-1 w-32 rounded focus:outline-none focus:border-indigo-300"
+                    />
+                  </div>
                 </div>
+              </template>
+            </v-date-picker>
+          </div>
+        </div>
+        <div class="filter-option guest-dropdown">
+          <div @click.stop="dropDownMenu($event)" class="add-guest-wrapper">
+            <label for="add-guest">Who</label>
+            <input disabled type="text" placeholder="Add guests" />
+          </div>
+
+          <div @click="formSubmit" class="order-container header-filter-search">
+            <div class="btn-container on-filter">
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="cell"></div>
+              <div class="content filter-content">
+                <button class="action-btn">
+                  <div class="material-icons search"> search </div>
+                  <span>Search</span>
+                </button>
               </div>
             </div>
           </div>
-        </el-form>
-      </div>
-    </Transition>
+        </div>
+      </el-form>
+    </div>
   </div>
 
   <div v-show="showModal" v-click-away="onClickAway" class="guests-modal dropdown-card order-container">
@@ -234,7 +238,11 @@
 <script>
   // import { ref } from 'vue';
   export default {
+    emits: ['expendForm'],
     name: 'explore-filter',
+    props: {
+      isExpend: Boolean,
+    },
     data() {
       return {
         filterBy: {
@@ -264,9 +272,8 @@
         let url = `/explore?location=${this.filterBy.where}`;
         this.$router.push(url);
       },
-      expendForm(ev) {
-        console.log('ev', ev.target.innerText);
-        console.log('ev', ev);
+      expendForm() {
+        this.$emit('expendForm', this.filterPreview);
         this.filterShow;
       },
       showInitModal(ev) {
