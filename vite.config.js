29e7bda8c125618fb0160173554256592f1bcc01
Nisan <nisani1239@gmail.com>
2022-07-26 13:54:31 +0300

    build server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { fileURLToPath, URL } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Airbnb-front/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -14,7 +13,6 @@ export default defineConfig({
   build: {
     outDir: "../backend/public",
     emptyOutDir: true,
-    assestsDir: "./dist",
-    chunkSizeWarningLimit: 1500
+    chunkSizeWarningLimit: 2500
   },
 })
