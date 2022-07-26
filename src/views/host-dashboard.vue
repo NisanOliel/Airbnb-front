a3dabba17864ac247221770c56c62dd877795931
Nisan <nisani1239@gmail.com>
2022-07-27 00:12:28 +0300

    add order

---
+++ src/views/host-dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <pre>{{ orders }}</pre>
+</template>
+
+<script>
+
+export default {
+  name: 'host-dashboard',
+  data() {
+    return {
+      orders: null
+    }
+  },
+  async created() {
+    this.orders = this.$store.getters.getOrders
+
+  },
+
+  computed: {
+
+  },
+}
+</script>
