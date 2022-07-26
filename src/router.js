37f1f04cc9bac43b08568a37c709e88d9ae2ab95
Nisan <nisani1239@gmail.com>
2022-07-26 22:59:55 +0300

    final

--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ import loginSignup from './views/login-signup.vue'
 import userDetails from './views/user-details.vue'
 import stayDetails from './views/stay-details.vue'
 import stayExplore from './views/stay-explore.vue'
-import hostDashboard from './views/host-dashboard.vue'
 
 
 const routes = [
@@ -34,12 +33,6 @@ const routes = [
     path: '/stay/:stayId',
     name: 'stay',
     component: stayDetails
-  },
-
-  {
-    path: '/dashboard',
-    name: 'host-dashboard',
-    component: hostDashboard
   },
 ]
 
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,92 +1,73 @@
 import { storageService } from './async-storage.service.js'
 import axios from 'axios'
-import { httpService } from "./http.service"
 
-// import ordersJason from '../../data/order.json' assert { type: 'json' }
+import ordersJason from '../../data/order.json' assert { type: 'json' }
 
-// const KEY = 'ordersDB'
+const KEY = 'ordersDB'
 // const API = '//localhost:3030/api/order/'
-// const API = process.env.NODE_ENV !== 'development' ? '/api/order/' : '//localhost:3030/api/order/'
 
-const API = 'order'
+const API = process.env.NODE_ENV !== 'development' ? '/api/order/' : '//localhost:3030/api/order/'
 
 export const orderService = {
     query,
     getById,
     remove,
     save,
-    // getEmptyorder,
+    getEmptyorder,
     getorder,
 }
 
-// _createorders()
+_createorders()
 
-async function query(filterBy = "") {
+function query(filterBy) {
     // return axios.get(API, { params: filterBy }).then((res) => res.data)
-    // return storageService.query(KEY)
-    console.log('API:', API)
-    console.log('filterBy:', filterBy)
-    return await httpService.get(API, filterBy)
-
+    return storageService.query(KEY)
 }
 
-async function getById(id) {
+function getById(id) {
     // return axios.get(API + id).then(res => res.data)
-    // return storageService.get(KEY, id)
-    return await httpService.get(`${API}/${id}`)
-
+    return storageService.get(KEY, id)
 }
 
-async function remove(id) {
+function remove(id) {
     // return axios.delete(API + id).then(res => res.data)
-    // return storageService.remove(KEY, id)
-    return await httpService.delete(API, id)
-
+    return storageService.remove(KEY, id)
 }
 
-async function save(order) {
+function save(order) {
     // if (order._id) {
     //     return axios.put(API + order._id, order).then(res => res.data)
     // } else {
     //     return axios.post(API, order).then(res => res.data)
     // }
 
-    // const savedOrder = (order._id) ? storageService.put(KEY, order) : storageService.post(KEY, order)
-    // return savedOrder
-    if (order._id) {
-        // return axios.put(API + order._id, order).then(res => res.data);
-        return await httpService.put(API, order)
-
-    } else {
-        return await httpService.post(API, order)
-        // return axios.post(API, order).then(res => res.data);
-    }
-
+    const savedOrder = (order._id) ? storageService.put(KEY, order) : storageService.post(KEY, order)
+    return savedOrder
 }
 
-// function getEmptyorder() {
-//     return {
-//         'hostId': '',
-//         'createdAt': '',
-//         'buyer': {
-//             '_id': '',
-//             'fullname': ''
-//         },
-//         'totalPrice': '',
-//         'startDate': '',
-//         'endDate': '',
-//         'guests': {
-//             'adults': '',
-//             'children': ''
-//         },
-//         'stay': {
-//             '_id': '',
-//             'name': '',
-//             'price': ''
-//         },
-//         'status': 'pending'
-//     }
-// }
+function getEmptyorder() {
+    return {
+        'hostId': '',
+        'createdAt': '',
+        'buyer': {
+            '_id': '',
+            'fullname': ''
+        },
+        'totalPrice': '',
+        'startDate': '',
+        'endDate': '',
+        'guests': {
+            'adults': '',
+            'children': ''
+        },
+        'stay': {
+            '_id': '',
+            'name': '',
+            'price': ''
+        },
+        'status': 'pending'
+    }
+}
 
 function getorder(orderId) {
     return storageService.get(KEY, orderId)
