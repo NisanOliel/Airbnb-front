dfe318de9e7e49d7dcc004da4079faa2d89bfd2d
Nisan <nisani1239@gmail.com>
2022-07-26 11:11:27 +0300

    github

--- src/router.js
+++ src/router.js
@@ -39,7 +39,7 @@ const routes = [
 
 export const router = createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHashHistory(process.env.NODE.ENV)
   // base: process.env.BASE_URL,
 })
 
