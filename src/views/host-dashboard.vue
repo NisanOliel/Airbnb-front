c8132cbace6df4e598bd5d260b098bf42f53ce88
Nisan <nisani1239@gmail.com>
2022-07-27 01:27:39 +0300

    dashboard

--- src/views/host-dashboard.vue
+++ src/views/host-dashboard.vue
@@ -1,6 +1,16 @@
 <template>
+  <div v-if="orders">
+    <div v-for="order in orders" :key="_id">
+      <span>{{ order.buyer.fullname }}</span>
+      <span>{{ order.totalPrice }}</span>
+    </div>
+    <!-- <span v-for="name in orders.stay.name">{{ orders.stay.name }}</span> -->
+    <!-- <pre>{{ orders.stay.name }}</pre> -->
+    <!-- <pre>{{ orders }}</pre> -->
+    <!-- <pre>{{ orders.stay.name }}</pre> -->
 
-  <pre>{{ orders }}</pre>
+    <img src="https://res.cloudinary.com/nisan/image/upload/v1658872030/air2b/unprofile_ji7zus.png" alt="" srcset="">
+  </div>
 </template>
 
 <script>
