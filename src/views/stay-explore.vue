313b2d6e4b0f75fb585ecc48f438b01d9a2c7655
Nisan <nisani1239@gmail.com>
2022-07-28 23:39:39 +0300

    explore unfilter and add stays nums

--- src/views/stay-explore.vue
+++ src/views/stay-explore.vue
@@ -1,43 +1,40 @@
 <template>
   <section class="container">
+    <h3> Stays: {{ getStays.length }}</h3>
     <stay-list :stays="getStays"></stay-list>
   </section>
-  <!-- <pre>{{ stays }}</pre> -->
 </template>
 
 <script>
-  import stayList from '../cmps/stay-list.cmp.vue';
+import stayList from '../cmps/stay-list.cmp.vue';
 
-  export default {
-    name: 'explore-page',
-    data() {
-      return {
-        stays: null,
-        filterBy: {},
-      };
+export default {
+  name: 'explore-page',
+  data() {
+    return {
+      filterBy: {},
+    };
+  },
+  computed: {
+    getStays() {
+      return this.$store.getters.getFilteredStays;
     },
-    computed: {
-      getStays() {
-        return this.$store.getters.getFilteredStays;
+  },
+  created() {
+    this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy });
+  },
+  watch: {
+    $route: {
+      handler() {
+        const { location } = this.$route.query;
+        this.filterBy.location = location;
       },
+      immediate: true,
     },
-    created() {
-      this.$store.dispatch({ type: 'setFilterBy', filterBy: this.filterBy });
-      // this.stays = this.$store.getters.getFilteredStays;
-    },
-    watch: {
-      $route: {
-        handler() {
-          const { location } = this.$route.query;
-          this.filterBy.location = location;
-          console.log(' this.$route.query:', this.$route.query);
-        },
-        immediate: true,
-      },
-    },
-    methods: {},
-    components: {
-      stayList,
-    },
-  };
+  },
+  methods: {},
+  components: {
+    stayList,
+  },
+};
 </script>
