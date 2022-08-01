b4ed12f723f1ce0f1c5ba669f2e8b0159419aa89
Nisan <nisani1239@gmail.com>
2022-08-01 15:12:05 +0300

    fix filter

--- src/cmps/orders-dashboard.vue
+++ src/cmps/orders-dashboard.vue
@@ -35,30 +35,6 @@
 
   </tr>
 
-  <!-- <tr>
-    <td>{{ new Date(hostOrder.createdAt).toLocaleString() }}</td>
-    <td class="td-image">{{ hostOrder.buyer.fullname }}</td>
-    <td>{{ hostOrder.stay.name }}</td>
-    <td>{{ hostOrder.startDate }}</td>
-    <td>{{ hostOrder.endDate }}</td>
-    <td>{{ hostOrder.status }}</td>
-    <td>{{ formatRevune }}</td>
-    <td>{{ hostOrder.guests.total }}</td>
-    <td>
-      <el-button v-if="hostOrder.status === 'pending'" @click.prevent="approve" size="small" type="success">Aprrove
-      </el-button>
-      <el-button v-if="hostOrder.status === 'pending'" @click.prevent="decline" size="small" type="danger">Decline
-      </el-button>
-      <el-button v-if="hostOrder.status === 'declined'" @click.prevent="approve" size="small" type="success">Aprrove
-      </el-button>
-      <el-button v-if="hostOrder.status === 'approved'" @click.prevent="decline" size="small" type="danger">Decline
-      </el-button>
-
-
-
-    </td>
-  </tr> -->
-
 </template>
 
 <script>
@@ -87,13 +63,10 @@ export default {
   },
   methods: {
     approve() {
-      // this.hostOrder.status = "approved";
-      // this.$store.dispatch({ type: "updateStay", order })
       const order = JSON.parse(JSON.stringify(this.hostOrder));
       this.$store.dispatch({ type: "saveOrder", order, status: 'approved' });
     },
     decline() {
-      // this.hostOrder.status = "declined";
       const order = JSON.parse(JSON.stringify(this.hostOrder));
       this.$store.dispatch({ type: "saveOrder", order, status: 'declined' });
     },
